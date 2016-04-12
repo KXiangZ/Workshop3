fc25c46788feb24f834f0572881c2d5ea0d678b0
jc321939 <freakclonetkx9946@gmail.com>
2016-04-12 13:31:02 +0800

    get_number function added

--- ascii.py
+++ ascii.py
@@ -1,6 +1,20 @@
+def get_number(lower,upper):
+    user_input=input("Enter a number ({} - {})".format(lower,upper))
+    while True:
+        if user_input.isdecimal():
+            if int(user_input) <upper and int(user_input) > lower:
+                break
+        print("Please enter a valid number!")
+        user_input=input("Enter a number ({} - {})".format(lower,upper))
+
+
+
+
 lower=10
 upper=100
 lower_input=int(input("Enter a number({}---{}):".format(lower,upper)).strip())
 upper_input=int(input("Enter a number({}---{}):".format(lower,upper)).strip())
 for i in range(lower_input,upper_input):
     print('{}  {}'.format(i,chr(i)))
+
+get_number(lower_input,upper_input)
